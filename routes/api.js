72ee282b8b410d87a122d4f36a8054c68847c23d
mramazzini <mramazzini123@gmail.com>
2022-10-07 11:51:47 -0500

    Added ID, Note Post

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 const api = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 api.get('/', (req, res) => {
     console.info(`${req.method} request received for api`);
@@ -6,7 +8,34 @@ api.get('/', (req, res) => {
   });
 
 api.get('/notes', (req, res) => {
-    readFromFile('./Develop/db/notes.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+
+api.post('/notes', (req, res) => {
+
+    const { title, text } = req.body;
+  
+
+    if (title && text) {
+
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './Develop/db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
   
 module.exports = api;
